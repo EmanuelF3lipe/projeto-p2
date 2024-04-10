56b0119b564d6393889eae1fcdd7750b61570e03
EmanuelF3lipe <emanuel.grepo@gmail.com>
2024-04-09 21:46:13 -0300

    enviandoClassSalve

---
+++ p2/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner escaner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = escaner.nextLine();
+        System.out.println("Olá! "+nome);
+    }
+}
