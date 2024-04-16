f2303893a20c7a5dc3c73f9f2ff407e20aa7c411
EmanuelF3lipe <emanuel.grepo@gmail.com>
2024-04-16 14:49:15 -0300

    enviandoModResumo

---
+++ miniprojeto-1/src/miniprojeto.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class miniprojeto {
+    public static void main(String[] args) throws Exception {
+
+        Scanner escaner = new Scanner(System.in);
+        
+        System.out.println("Olá, para começarmos me diga seu nome: ");
+        String nomeP = escaner.nextLine();
+    }
+}
